5e9bbb6e4e3b03f7ac8ae7b2849de62ed89c0d80
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-15 15:50:24 -0500

    Fixed if statement where  value is larger than  value

--- for.php
+++ for.php
@@ -10,12 +10,24 @@ do {
 	$end = trim(fgets(STDIN));
 } while (!is_numeric($end));
 
-
 fwrite(STDOUT, 'Enter numeric incremental value: ');
 $countBy = trim(fgets(STDIN));
 
 if (empty($countBy) || !is_numeric($countBy)){
 	$countBy = 1;
+}
+
+// if ending is less than start, swap values
+if ($end < $start) {
+	$oldEnd = $start;
+	$oldStart = $end;
+	$start = $oldStart;
+	$end = $oldEnd;
+}
+
+// if incrementer is negative, then change it to a positive w/ absolute value
+if ($countBy < 0) {
+	$countBy = abs($countBy);
 }
 
 for ($i = $start; $i <= $end ;$i += $countBy) {
