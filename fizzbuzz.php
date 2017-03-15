0c14db2560695170710e054b5f8fb2eb62f23dbc
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-15 10:24:59 -0500

    Created first else if statement to show  % 5 === 0 is equal to the string Buzz

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,9 +1,10 @@
 <?php 
 
 for ($i = 1; $i <= 100 ;$i++) {
-	// process: modulus
 	if ($i % 3 === 0 && $i % 5 === 0) {
 		fwrite(STDOUT, 'FizzBuzz' . PHP_EOL);
+	} else if ($i % 5 === 0) {
+		fwrite(STDOUT, 'Buzz' . PHP_EOL);
 	} else {
 		echo $i . PHP_EOL;
 	}
