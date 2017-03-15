19c496467535024a9275bd11613c3d8ae4a6c88a
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-15 10:49:40 -0500

    Created do while loops to catch any non numeric values entered for start and end variables and ask user to add numeric values to continue to for loop.

--- for.php
+++ for.php
@@ -1,11 +1,21 @@
 <?php 
 
-//input: A pair of numbers: min and max for a range
 fwrite(STDOUT, 'Enter the start of the range ');
 $start = trim(fgets(STDIN));
 
+do {
+	fwrite(STDOUT, 'Enter numeric start value ');
+	$start = trim(fgets(STDIN));
+
+} while (!is_numeric($start));
+
 fwrite(STDOUT, 'Enter the end of the range ');
 $end = trim(fgets(STDIN));
+
+do {
+	fwrite(STDOUT, 'Enter numeric end value ');
+	$end = trim(fgets(STDIN));
+} while (!is_numeric($end));
 
 for ($i = $start; $i <= $end ;$i++) {
 	echo $i . PHP_EOL;
