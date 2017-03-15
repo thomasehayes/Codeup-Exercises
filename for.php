34f0a1dc9fab33b95c1837a104d1df28025b9c81
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-15 11:04:23 -0500

    Created new variable $countBy to add incremental value

--- for.php
+++ for.php
@@ -1,23 +1,24 @@
 <?php 
 
-fwrite(STDOUT, 'Enter the start of the range ');
-$start = trim(fgets(STDIN));
-
 do {
 	fwrite(STDOUT, 'Enter numeric start value ');
 	$start = trim(fgets(STDIN));
-
 } while (!is_numeric($start));
-
-fwrite(STDOUT, 'Enter the end of the range ');
-$end = trim(fgets(STDIN));
 
 do {
 	fwrite(STDOUT, 'Enter numeric end value ');
 	$end = trim(fgets(STDIN));
 } while (!is_numeric($end));
 
-for ($i = $start; $i <= $end ;$i++) {
+
+fwrite(STDOUT, 'Enter numeric incremental value ');
+$countBy = trim(fgets(STDIN));
+
+if (!is_numeric($countBy)){
+	$countBy = 1;
+}
+
+for ($i = $start; $i <= $end ;$i += $countBy) {
 	echo $i . PHP_EOL;
 }
 ?>
