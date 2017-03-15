107e22d8ccfa4579e0ce1988605c0694a6a2b4df
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-15 09:56:47 -0500

    fixed misspelled FizzBuzz string and created new else if line to show  % 5 === 0 to show Buzz

--- for.php
+++ for.php
@@ -3,7 +3,9 @@
 for ($i = 1; $i <= 100 ;$i++) {
 	// process: modulus
 	if ($i % 3 === 0 && $i % 5 === 0) {
-		fwrite(STDOUT, 'FizzBuss' . PHP_EOL);
+		fwrite(STDOUT, 'FizzBuzz' . PHP_EOL);
+	} else if ($i % 5 === 0) {
+		fwrite(STDOUT, 'Buzz' . PHP_EOL);
 	} else {
 		echo $i . PHP_EOL;
 	}
