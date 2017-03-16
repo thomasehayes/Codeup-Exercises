b6eb52b8716e155b0a8f0567087b5c62174da36f
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-16 14:54:43 -0500

    replaced variable names for better naming convention and added notes from ryan's lecture.

--- foreach_books.php
+++ foreach_books.php
@@ -26,9 +26,19 @@ $books = [
 foreach ($books as $title => $book) {
     if ($book['published'] > 1950){
         echo $title . PHP_EOL;
-        foreach ($book as $info => $value) {
-           echo "$info: $value" . PHP_EOL;
+        foreach ($book as $bookInfo => $bookValue) {
+           echo "\t $bookInfo: $bookValue" . PHP_EOL;
         }
     }
 }
+
+//====================
+//   Note Section
+//====================
+//if you have arrays, you will use foreach loops to get into it. 
+//if you run into array to string conversion, it means you're trying to echo an array and its trying to convert it to a string, so you won't see the data within the array. 
+// .= is concantenating and assigning it to the variable
+//how do we filter stuff? 
+    // use a if statement
+
 ?>
