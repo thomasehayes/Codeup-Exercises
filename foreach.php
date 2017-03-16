b391c0879d7e5da3ca4738d9671e2f3a4b34700a
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-16 10:49:14 -0500

    in the foreach.php file, I included luis' solution

--- foreach.php
+++ foreach.php
@@ -2,10 +2,34 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $thing) {
-	if (is_scalar($thing)){
-		echo $thing . PHP_EOL; 
-	} 
-}
+// foreach ($things as $thing) {
+// 	if (is_scalar($thing)){
+// 		echo $thing . PHP_EOL; 
+// 	} 
+// 	if (is_null($thing)) {
+// 		echo $thing . PHP_EOL;
+// 	}
+// 	if (is_array($thing)) {
+// 		echo "Array "; 
+// 		foreach ($thing as $innerThing) {
+// 			echo $innerThing . " ";
+// 		}
+// 		echo PHP_EOL;
+// 	}
+// }
 
+
+foreach ($things as $thing) {
+    if(is_array($thing)) {
+        // print_r($thing);
+        // display array like Array (1, 2, 3)
+        echo 'Array ( ';
+        foreach ($thing as $innerThing) {
+            echo $innerThing . " ";
+        }
+        echo ')', PHP_EOL;
+    } else {
+        echo $thing, PHP_EOL;
+    }
+}
 ?>
---
+++ foreach_books.php
@@ -0,0 +1,31 @@
+<?php 
+
+$books = [
+    'The Hobbit' => [
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ],
+    'Game of Thrones' => [
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ],
+    'The Catcher in the Rye' => [
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ],
+    'A Tale of Two Cities' => [
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    ]
+];
+
+foreach ($books as $book) {
+    foreach ($book as $key => $value) {
+        echo "$key: $value" . PHP_EOL;
+    }
+}
+?>
