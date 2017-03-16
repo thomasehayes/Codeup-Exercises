d540b89cfabb0ce2cfb9306caf6dd58252894e5b
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-16 16:14:32 -0500

    Added 2 foreach loops. One to search for states that begin with a vowel and ends with a vowel and pushing it into an empty array variable. The second is to echo out the array with key values.

--- states.php
+++ states.php
@@ -1,8 +1,23 @@
 <?php 
-// Using the following associative array, produce a script that does the following:
+// Exercise: Using the following associative array, produce a script that does the following:
 // 	- Outputs only the states with an "x" character in the state name
 // 	- Outputs all the states without the letter "a" in their name
 // 	- Outputs the states and abbreviations of all the states starting with vowels.
+
+// Exercises Part 2
+// Use a foreach to make a new array containing states that start with and end with vowels
+	// call the array $statesStartingAndEndingWithVowels
+	// echo "These are states starting and ending with vowels"
+	// then echo each state name in this new array
+
+// use a foreach to construct a new array containing states with names that are more than one word.
+	// call the array $statesWithMoreThanOneWordNames
+	// echo "These are the states with more than one word in their name"
+	// then echo each state name below
+
+// use a foreach to construct a new array of all the states with "North" "East" "South" or "West"
+	// call the array $arrayOfCardinalStates
+	// echo "These are states with north, south, east, or west in their name"
 
 $states = [
     'AL' => 'Alabama',
@@ -75,10 +90,24 @@ foreach ($states as $state => $stateName) {
 echo "----------". PHP_EOL;
 
 foreach ($states as $state => $stateName) {
-	$firstLetter = substr($state, 0, 1);
-	if ($firstLetter=='A' || $firstLetter=='E' || $firstLetter=='I' || $firstLetter=='O' ||$firstLetter=='U'){
+	$firstLetter = substr($stateName, 0, 1);
+	if ($firstLetter === 'A' || $firstLetter === 'E' || $firstLetter === 'I' || $firstLetter === 'O' ||$firstLetter === 'U'){
 		echo $state . "\t $stateName" . PHP_EOL;
 	}
 }
+echo "----------". PHP_EOL;
+
+$statesStartingAndEndingWithVowels = [];
+foreach ($states as $state => $stateName) {
+	$firstLetter = substr($stateName, 0, 1);
+	$lastLetter = substr($stateName, -1, 1);
+	if (($firstLetter === 'A' || $firstLetter === 'E' || $firstLetter === 'I' || $firstLetter === 'O' ||$firstLetter === 'U' || $firstLetter === 'Y') && ($lastLetter === 'a' || $lastLetter === 'e' || $lastLetter === 'i' || $lastLetter === 'o' ||$lastLetter === ' u' || $lastLetter === 'y')){
+		$statesStartingAndEndingWithVowels[$state] = $stateName;
+	}
+}
+foreach($statesStartingAndEndingWithVowels as $state => $stateName) {
+	echo $state . "\t $stateName" . PHP_EOL;  
+}
+
 
 ?>
