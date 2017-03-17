b20f9e4c1556a761e2021d4b69afdab663463067
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-17 15:13:48 -0500

    Added returns to each function and echoed them out to see if they are outputting correctly.

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b . PHP_EOL;
+}
+
+function multiply($a, $b)
+{
+	return $a * $b . PHP_EOL;
+}
+
+function divide($a, $b)
+{
+    return $a / $b . PHP_EOL;
+}
+
+echo add(6, 3);
+echo subtract(6, 3);
+echo multiply(6, 3);
+echo divide(6, 3);
+// Add code to test your functions here
