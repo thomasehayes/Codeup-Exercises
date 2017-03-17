813c51c9b74adb2619ec047564df724da4a6e349
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-17 15:17:12 -0500

    Added modulus function. Outputs correctly.

--- arithmetic.php
+++ arithmetic.php
@@ -20,8 +20,13 @@ function divide($a, $b)
     return $a / $b . PHP_EOL;
 }
 
+function modulus($a, $b) {
+	return $a % $b . PHP_EOL;
+}
+
 echo add(6, 3);
 echo subtract(6, 3);
 echo multiply(6, 3);
 echo divide(6, 3);
+echo modulus(5, 3);
 // Add code to test your functions here
