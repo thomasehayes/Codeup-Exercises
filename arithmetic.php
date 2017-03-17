3554dc24c15ae9b5292f4e4507868cd92395ebd0
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-17 15:33:48 -0500

    Added conditionals to only allow numeric values in arguments or they get an error message.

--- arithmetic.php
+++ arithmetic.php
@@ -3,33 +3,49 @@
 $a = 10;
 $b = 2;
 
-function add($a, $b)
-{
-    return $a + $b . PHP_EOL;
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a + $b . PHP_EOL;
+	} else {
+		echo "Both arguments need to be numeric". PHP_EOL;
+	}
 }
 
-function subtract($a, $b)
-{
-    return $a - $b . PHP_EOL;
+function subtract($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a - $b . PHP_EOL;
+	} else {
+		echo "Both arguments need to be numeric.". PHP_EOL;
+	}
 }
 
-function multiply($a, $b)
-{
-	return $a * $b . PHP_EOL;
+function multiply($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a * $b . PHP_EOL;
+	} else {
+		echo "Both arguments need to be numeric.". PHP_EOL;
+	}
 }
 
-function divide($a, $b)
-{
-    return $a / $b . PHP_EOL;
+function divide($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a / $b . PHP_EOL;
+	} else {
+		echo "Both arguments need to be numeric". PHP_EOL;
+	}
 }
 
 function modulus($a, $b) {
-	return $a % $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a % $b . PHP_EOL;
+	} else {
+		echo "Both arguments need to be numeric". PHP_EOL;
+	}
 }
 
-echo add(6, 3);
-echo subtract(6, 3);
-echo multiply(6, 3);
-echo divide(6, 3);
-echo modulus(5, 3);
+echo add(3, 3);
+echo subtract(3, 3);
+echo multiply(3, 3);
+echo divide(3, 3);
+echo modulus(3, 3);
 // Add code to test your functions here
