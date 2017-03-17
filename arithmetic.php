8e2c05f4ed4b5f67c977422489f8d284bb2efceb
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-17 16:04:15 -0500

    refactored error messages into a function

--- arithmetic.php
+++ arithmetic.php
@@ -7,7 +7,7 @@ function add($a, $b) {
 	if (is_numeric($a) && is_numeric($b)){
     	return $a + $b . PHP_EOL;
 	} else {
-		echo "Both arguments need to be numeric". PHP_EOL;
+		echo throwErrorMessage($a, $b);
 	}
 }
 
@@ -15,7 +15,7 @@ function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)){
     	return $a - $b . PHP_EOL;
 	} else {
-		echo "Both arguments need to be numeric.". PHP_EOL;
+		echo throwErrorMessage($a, $b);
 	}
 }
 
@@ -23,19 +23,17 @@ function multiply($a, $b) {
 	if (is_numeric($a) && is_numeric($b)){
     	return $a * $b . PHP_EOL;
 	} else {
-		echo "Both arguments need to be numeric.". PHP_EOL;
+		echo throwErrorMessage($a, $b);
 	}
 }
 
 function divide($a, $b) {
 	if ($b === 0){
-		echo "Cannot divide by 0." . PHP_EOL;
-		echo "Value inputed for division: " . $a . PHP_EOL;
-		echo "Value inputed for division: " . $b . PHP_EOL;
+		return "Cannot divide by 0." . PHP_EOL;
 	} else if (is_numeric($a) && is_numeric($b)) {
     	return $a / $b . PHP_EOL;
 	} else {
-		echo "Both arguments need to be numeric". PHP_EOL;
+		echo throwErrorMessage($a, $b);
 	}	
 }
 
@@ -43,13 +41,17 @@ function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)){
     	return $a % $b . PHP_EOL;
 	} else {
-		echo "Both arguments need to be numeric". PHP_EOL;
+		echo throwErrorMessage($a, $b);
 	}
+}
+
+function throwErrorMessage($a, $b) {
+	return "Both arguments need to be numeric. Values entered were $a and $b ". PHP_EOL;
 }
 
 echo add(3, 3);
 echo subtract(3, 3);
 echo multiply(3, 3);
 echo divide(3, 0);
-echo modulus(3, 3);
+echo modulus("w", 3);
 // Add code to test your functions here
