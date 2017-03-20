3d99b8ba5f11c074a29b6d3a99278c86e19f3164
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-20 12:16:26 -0500

    Created function to search for names to see if it is in the  array

--- search-arrays.php
+++ search-arrays.php
@@ -4,12 +4,33 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+$nameSearch = 'Tina';
 
-$resultTina = array_search('Tina', $names, true);
-var_dump($resultTina);
+// $resultTina = array_search('Tina', $names, true);
+// var_dump($resultTina);
 
-$resultBob = array_search('Bob', $names, true);
-var_dump($resultBob);
+// $resultBob = array_search('Bob', $names, true);
+// var_dump($resultBob);
+
+
+//Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+
+function search($name, $array) {
+	$result = array_search($name, $array);
+	if($result !== false) {
+		return "True" . PHP_EOL;
+	} else {
+		return "False" . PHP_EOL;
+	}
+}
+
+echo search($nameSearch, $names);
+
+// $result = array_intersect($names, $compare);
+// print_r($result);
+
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search()
 
 
 ?>
