9b99e085d0f84af8e3cc658a429d0a1b19ec45f6
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-21 13:35:54 -0500

    exercise with solution

--- contact-parser.php
+++ contact-parser.php
@@ -1,19 +1,27 @@
 <?php
-$file = "contacts.txt";
-$handle = fopen($file, 'r');
-$contents = fread($handle, filesize($file));
 
 function parseContacts($contents)
 {
+	$contacts = array(); 
+	// Open 
+	$file = "contacts.txt";
+	$handle = fopen($file, 'r');
+	$contents = fread($handle, filesize($file));
+	$person = explode("\n", $contents);
 
-	$testArray = explode("\n", $contents);
-	foreach ($testArray as $key) {
-		$val = explode("|", $key);
-		$desiredArray[$val[0]] = $val[1];
+	foreach ($person as $number) {
+		$info = explode("|", $number);
+		$info[1] = substr($info[1], 0, 3) . "-" . substr($info[1], 3, 3) . "-" . substr($info[1], 6);
+		$eachContact = [
+			"name" => $info[0],
+			"number" => $info[1]
+		];
+		$contacts[] = $eachContact;
+
+	}
+
+	fclose($handle);
+    return $contacts;
 }
 
-    return $desiredArray;
-}
-
-print_r(parseContacts($contents));
-fclose($handle);
+var_dump(parseContacts('contacts.txt'));
