6baef260cc7f86d949afb5f9672bfff671bfef52
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-21 10:12:29 -0500

    added solutions to exercise

--- list_with_commas.php
+++ list_with_commas.php
@@ -2,11 +2,58 @@
 
  // Converts array into list n1, n2, ..., and n3
  function humanizedList($array) {
- 	$finalArray = array_pop($array);
- 	$final = array_push($array, "and ") . implode(",", $array);
- 	return $final;
-
+ 	// $finalArray = array_pop($array);
+ 	// $final = array_push($array, "and ") . implode(",", $array);
+ 	// return $final;
+ 	$output = "";
+ 	$lastValue = array_pop($array);
+ 	$output = implode("," , $array);
+ 	$output .= ", and $lastValue";
+ 	
+ 	return $output;
  }
+
+// Humanize list with sorting option
+ function humanizedListSort($array, $sortName = false) {
+ 	if ($sortName == true) {
+ 		sort($array);
+ 	}
+
+ 	$output = "";
+ 	$lastValue = array_pop($array);
+ 	$output = implode("," , $array);
+ 	$output .= ", and $lastValue";
+ 	
+ 	return $output;
+ }
+
+function humanizedFirstName($array, $sortName = false) {
+
+	$output = "";
+	//Eliminate last names
+
+	$names = implode(" ", $array);
+	$namesArray = explode(" ", $names);
+	$firstNamesArray = [];
+	for($i = 0; $i <count($namesArray); $i +=2){
+		array_push($firstNamesArray, $namesArray[$i]);
+	}
+
+	if ($sortName == true) {
+		sort($firstNamesArray);
+	}
+
+	$lastValue = array_pop($firstNamesArray);
+	$output = implode(",", $firstNamesArray);
+	$output .= ", and $lastValue";
+
+	return $output;
+}
+
+
+
+
+
 
 
  // List of famous peeps
@@ -14,10 +61,12 @@
 
  // TODO: Convert the string into an array
  $physicistsArray = explode(",", $physicistsString);
- print_r($physicistsArray);
+ // print_r($physicistsArray);
 
  // Humanize that list
- $famousFakePhysicists = humanizedList($physicistsArray);
+$famousFakePhysicists = humanizedList($physicistsArray);
+$famousFakePhysicists = humanizedListSort($physicistsArray);
+$famousFakePhysicists = humanizedFirstName($physicistsArray);
 
  // Output sentence
  echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
