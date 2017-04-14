3f1063a48dd5afd7eea18551c49f1570dce3a982
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-14 14:26:36 -0500

    construct function only allows one parameter to ensure no user errors in numeric values and added perimeter function

--- Square.php
+++ Square.php
@@ -2,6 +2,17 @@
 
 require_once 'Rectangle.php';
 
-class Square extends Rectangle;
+class Square extends Rectangle {
+
+	public function __construct ($height) {
+		return $this->height = $height;
+	}
+
+	public function perimeter() {
+		echo "Perimeter of a square: ";
+		return $this->height * 4;
+	}
+}
+
 
 ?>
