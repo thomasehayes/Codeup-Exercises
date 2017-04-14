b173df4e7cd0a288dfeba78c36d1ca4bf90f87a7
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-14 14:12:24 -0500

    Completed 1-3 in exercise 9.2.1

---
+++ Rectangle.php
@@ -0,0 +1,21 @@
+<?php  
+
+class Rectangle {
+	public $height;
+	public $width;
+
+	public function __construct($height, $width) {
+		if(!is_numeric($height) || !is_numeric($width)) {
+			echo "Try again with numeric values!";
+		} else {
+			$this->height = $height;
+			$this->width = $width;
+		}
+	}
+	public function areaOfRectangle() {
+	
+		return ($this->height) * ($this->width);
+	
+	}
+}
+?>
